f6601cc62aa02aa500d94d671a75c3298fcf5ab2
Rebecca Agyapong <ragyapon@uci.edu>
2024-08-12 13:54:56 -0700

    dictuple class almost complete

--- .venv/lib/dicttuple.py
+++ .venv/lib/dicttuple.py
@@ -4,3 +4,31 @@
 2. defined an __init__ method that takes one parameter
 3.
 """
+
+class DictTuple:
+    def __init__(self,*args):
+        self.dt = args
+    def __len__(self):
+        key_counter = 0
+        for key in self.dt:
+            key_counter += len(key.keys())
+        return key_counter
+    def __bool__(self):
+        if len(self.dt) == 1:
+            return False
+        else:
+            return True
+    def __repr__(self):
+        return 'DictTuple({})'.format(','.join(str(d) for d in self.dt))
+
+    def _contains__(self):
+    # if k is in self.dt:
+        #return True
+    #else:
+        #return False
+
+    def __iter__(self):
+        pass
+    def __eq__(self, other):
+        pass
+
--- mynamedtuple.py
+++ mynamedtuple.py
@@ -34,10 +34,11 @@
 def mynamedtuple(type_name, field_name, mutable=False, defaults={}):
     pass
 class NamedTuple:
-    def __init__(self,type_name,field_name):
+    def __init__(self,type_name,field_name,mutable, defaults):
         self.type_name = type_name
         self.field_name = field_name
-        self.defaults = {}
+        self.mutable = False
+        self.defaults ={}
     def __repr__(self):
         return f"NamedTuple(x={self.type_name},y={self.field_name})"
     def __getitem__(self,index):
@@ -45,15 +46,40 @@ class NamedTuple:
             return self.type_name
         elif index == 1:
             return self.field_name
+    def __eq__(self, other):
+        if isinstance(other, self.__class__):
+            return self.type_name == other.type_name and self.field_name == other.field_name
+        return False
     def __asdict__(self):
-        pass
-    def __make__(self):
-        pass
-    def __replace__(self,**kwargs):
-        pass
-    def __setattr__(self):
-        pass
+        return {"type_name":self.type_name,"field_name":self.field_name}
+    def __make__(self,values):
+        my_dict = {}
+        for i in range(len(self.field_name)):
+            key = self.field_name[i]
+            value = values[i]
+            my_dict[key] = value
+        return NamedTuple(self.type_name,self.field_name,self.mutable,my_dict)
+    def __replace__(self,**kargs):
+        # kwargs is  y= 5
+        # defaults is x =0 y =0
+        if self.mutable == True:
+            for k,v in self.defaults.items():
+                if k in self.defaults.keys():
+                    self.defaults[k] = kargs
+    def __setattr__(self,name,value):
+        # check if mutable parameter is false
+        # the namedtuple will not allow instance names to be changed
+        # raise AttributeError with and appropriate message
+        if self.mutable == True:
+            raise AttributeError("Cannot modify because value is immutable")
+        else:
+            self.defaults[name] = value
 
 
 
 
+
+
+
+
+obj = NamedTuple("h")
