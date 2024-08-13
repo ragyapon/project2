667789fb7780ed222619ca6147775affea6b5a4b
Rebecca Agyapong <ragyapon@uci.edu>
2024-08-12 21:07:35 -0700

    fix bugs

--- mynamedtuple.py
+++ mynamedtuple.py
@@ -29,48 +29,6 @@
 
  13. __setattr__ if the mutable parameter is False will raise AttrributeError
 """
-# class NamedTuple:
-#     def __init__(self,type_name,field_name,mutable, defaults):
-#         self.type_name = type_name
-#         self.field_name = field_name
-#         self.mutable = False
-#         self.defaults ={}
-#     def __repr__(self):
-#         return f"NamedTuple(x={self.type_name},y={self.field_name})"
-#     def __getitem__(self,index):
-#         if index == 0:
-#             return self.type_name
-#         elif index == 1:
-#             return self.field_name
-#     def __eq__(self, other):
-#         if isinstance(other, self.__class__):
-#             return self.type_name == other.type_name and self.field_name == other.field_name
-#         return False
-#     def __asdict__(self):
-#         return {"type_name":self.type_name,"field_name":self.field_name}
-#     def __make__(self,values):
-#         my_dict = {}
-#         for i in range(len(self.field_name)):
-#             key = self.field_name[i]
-#             value = values[i]
-#             my_dict[key] = value
-#         return NamedTuple(self.type_name,self.field_name,self.mutable,my_dict)
-#     def __replace__(self,**kargs):
-#         # kwargs is  y= 5
-#         # defaults is x =0 y =0
-#         if self.mutable == True:
-#             for k,v in self.defaults.items():
-#                 if k in self.defaults.keys():
-#                     self.defaults[k] = kargs
-#     def __setattr__(self,name,value):
-#         # check if mutable parameter is false
-#         # the namedtuple will not allow instance names to be changed
-#         # raise AttributeError with and appropriate message
-#         if self.mutable == True:
-#             raise AttributeError("Cannot modify because value is immutable")
-#         else:
-#             self.defaults[name] = value
-#
 class NamedTuple:
     def __init__(self, type_name, field_name, mutable=False, defaults=None):
         self.type_name = type_name
@@ -108,12 +66,10 @@ class NamedTuple:
 
     def __replace__(self, **kargs):
         if self.mutable:
-            # Update defaults with new values
             for k, v in kargs.items():
                 if k in self.defaults:
                     self.defaults[k] = v
         else:
-            # Return a new instance with updated values
             new_defaults = self.defaults.copy()
             new_defaults.update(kargs)
             return NamedTuple(self.type_name, self.field_name, self.mutable, new_defaults)
